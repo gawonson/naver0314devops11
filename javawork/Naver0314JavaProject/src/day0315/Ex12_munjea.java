de228b570fbc0cc9494256ff9a90a7efe81c12be
gawonson <ok466sgw@naver.com>
2024-03-15 17:31:48 +0900

    3월15일수업

--- javawork/Naver0314JavaProject/src/day0315/Ex12_munjea.java
+++ javawork/Naver0314JavaProject/src/day0315/Ex12_munjea.java
@@ -10,20 +10,20 @@ public class Ex12_munjea {
 		
 		
 		int score;
-		System.out.println("점수");
-	
-		score = sc.nextInt();
+		String msg;
 		String msg90 = "참 잘했어요!!";
 		String msg80 = "수고했어요!";
 		String msg70 = "조금 더 노력하세요";
 		String msg60 = "재시험입니다!";
 		
-		String msg;
+		System.out.println("점수");
+		score = sc.nextInt();
 		
 		msg=score>=90?msg90: //msg=score>=90?"참 잘했어요!!":score>=80?...
 			score>=80?msg80:
 				score>=70?msg70:
 					msg60;
+				
 		System.out.println(score+"=>"+msg);
 				
 	}
